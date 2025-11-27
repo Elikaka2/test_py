1918dc562bf112260af79424f301604bfd30afac
Danil <elikaka.sad@mail.ru>
2025-11-27 18:19:12 +0300

    hotfix

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserRead
 from app.services.user_service import create_user
 from app.database import get_db
+from app.models.user import User
 
 router = APIRouter()
 
 @router.post("/create", response_model=UserRead)
 def create_user_endpoint(user: UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(UserRead).filter_by(username=user.username).first()
+    existing_user = db.query(User).filter_by(username=user.username).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
     return create_user(db, user.username, user.password)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
-from passlib.hash import bcrypt
 from app.models.user import User
+import bcrypt
 
 def create_user(db: Session, username: str, password: str) -> User:
-    password_hash = bcrypt.hash(password)
+    password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     user = User(username=username, password_hash=password_hash)
     db.add(user)
     db.commit()
